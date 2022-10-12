a6fba70f3fb65f6709c7e1be8b1da1b1102c536e
Shanathory <55476699+Shanathory@users.noreply.github.com>
2022-10-12 20:11:51 -0300

    PlayerController - Enviroment - Build 1
    Se agrego la funcionalidad de moverse y saltar en un enviroment

--- JumpJumpAdventure/Assets/Script/CharacterCotroller.cs
+++ JumpJumpAdventure/Assets/Script/CharacterCotroller.cs
@@ -4,35 +4,53 @@ using UnityEngine;
 
 public class CharacterCotroller : MonoBehaviour
 {
+    private new Rigidbody2D rigidbody;//Instansiamos la variable rigidbody.
     public float velocidadMovimiento;
-  
+    private bool mirandoDerecha = true;
+    public float fuerzaSalto;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        /* obtenemos el componente que tiene
+         * el objeto, que es el Rigidbody2D.
+         */
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcesarMovimiento();
+        ProcesarSalto();
     }
 
+    void ProcesarSalto()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigidbody.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+        }
+    }
     void ProcesarMovimiento()
     {
-       
         /*
-         * Los GetAxis son funciones dentro del InputManager y funcionan con un valo rpositivo y el
-         * mismo valor, pero negativo. En caso de este input funciona con las flechitas.
+         * Los GetAxis son funciones dentro del InputManager y funcionan con un valo rpositivo (1) y el
+         * mismo valor, pero negativo (-1). En caso de este input funciona con las flechitas.
          * Lo utilizaremos para poder mover la personaje.
          */
         float inputMovimiento = Input.GetAxis("Horizontal");
-    
-        /*
-         * Instansiamos la variable rigidbody y obtenemos el componente que tiene
-         * el objeto, que es el Rigidbody2D.
-         */
-        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
         rigidbody.velocity = new Vector2(inputMovimiento * velocidadMovimiento, rigidbody.velocity.y); //Mueve al personaje en el eje X.
+        
+        GestionarOrientacion(inputMovimiento);
+    }
+
+    void GestionarOrientacion(float inputMovimiento)
+    {
+        if ( (mirandoDerecha == true && inputMovimiento < 0) || (mirandoDerecha == false && inputMovimiento > 0) )
+        {
+            mirandoDerecha = !mirandoDerecha; //Si es true lo coloca a false y si es false lo cambia a true.
+            transform.localScale = new Vector2(-1 * transform.localScale.x, transform.localScale.y); //Rota al personaje 180
+        }
     }
 }
